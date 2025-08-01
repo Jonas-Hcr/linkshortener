e015913d43a9b17a5ff1a7aa24774889026fd118
Jonas Henrique <programeicafe@outlook.com>
2025-07-31 21:43:21 -0300

    feat: implement ShortLink service and controller with URL shortening functionality

---
+++ src/main/java/br/com/jonashcr/linkshortener/dtos/UrlDTO.java
@@ -0,0 +1,4 @@
+package br.com.jonashcr.linkshortener.dtos;
+
+public record UrlDTO(String url) {
+}
--- src/main/java/br/com/jonashcr/linkshortener/repositories/ShortLinkRepository.java
+++ src/main/java/br/com/jonashcr/linkshortener/repositories/ShortLinkRepository.java
@@ -3,5 +3,8 @@ package br.com.jonashcr.linkshortener.repositories;
 import br.com.jonashcr.linkshortener.domain.shortlink.ShortLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShortLinkRepository extends JpaRepository<ShortLink, String> {
+    Optional<ShortLink> findByShortCode(String shortCode);
 }
