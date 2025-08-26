6b9b6f714c1fbbf1777f106c57387a919a949495
Jonas Henrique <programeicafe@outlook.com>
2025-08-25 22:07:09 -0300

    feat: configure test profile and import TestConfig for application tests

--- src/test/java/br/com/jonashcr/linkshortener/LinkshortenerApplicationTests.java
+++ src/test/java/br/com/jonashcr/linkshortener/LinkshortenerApplicationTests.java
@@ -1,13 +1,18 @@
 package br.com.jonashcr.linkshortener;
 
+import br.com.jonashcr.linkshortener.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestConfig.class)
 class LinkshortenerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
